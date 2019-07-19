b2ff16b7b1e783586f15589ef89bfe7e3f27694b
Jake Humphrey <jakehumphrey18@gmail.com>
2019-07-19 22:02:06 +0200

    static version

---
+++ assets/js/cash-daily-paralax.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+  var s = skrollr.init({
+		edgeStrategy: 'set',
+		easing: {
+			WTF: Math.random,
+			inverted: function(p) {
+				return 1-p;
+			}
+		}
+	});
+});
--- assets/js/cash-daily.js
+++ assets/js/cash-daily.js
@@ -1,54 +1,44 @@
 $( document ).ready(function() {
-  var s = skrollr.init({
-		edgeStrategy: 'set',
-		easing: {
-			WTF: Math.random,
-			inverted: function(p) {
-				return 1-p;
-			}
-		}
-	});
-
   var recurring = false;
-  $('#recurring-details-button').click(function() {
+  $('.recurring-details-button').click(function() {
     if (recurring) {
-      $('#recurring-details').fadeTo('slow', 0);
+      $('.recurring-details').fadeTo('slow', 0);
       recurring = false;
     } else {
-      $('#recurring-details').fadeTo('slow', 1);
+      $('.recurring-details').fadeTo('slow', 1);
       recurring = true;
     }
   });
 
   var customizable = false;
-  $('#highly-customizable-details-button').click(function() {
+  $('.highly-customizable-details-button').click(function() {
     if (customizable) {
-      $('#highly-customizable-details').fadeTo('slow', 0);
+      $('.highly-customizable-details').fadeTo('slow', 0);
       customizable = false;
     } else {
-      $('#highly-customizable-details').fadeTo('slow', 1);
+      $('.highly-customizable-details').fadeTo('slow', 1);
       customizable = true;
     }
   });
 
   var daily = false;
-  $('#daily-details-button').click(function() {
+  $('.daily-details-button').click(function() {
     if (daily) {
-      $('#daily-details').fadeTo('slow', 0);
+      $('.daily-details').fadeTo('slow', 0);
       daily = false;
     } else {
-      $('#daily-details').fadeTo('slow', 1);
+      $('.daily-details').fadeTo('slow', 1);
       daily = true;
     }
   });
 
   var scenarios = false;
-  $('#scenarios-details-button').click(function() {
+  $('.scenarios-details-button').click(function() {
     if (scenarios) {
-      $('#scenarios-details').fadeTo('slow', 0);
+      $('.scenarios-details').fadeTo('slow', 0);
       scenarios = false;
     } else {
-      $('#scenarios-details').fadeTo('slow', 1);
+      $('.scenarios-details').fadeTo('slow', 1);
       scenarios = true;
     }
   });
